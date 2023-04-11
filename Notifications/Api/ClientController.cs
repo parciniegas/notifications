643deaab6b722f9bb465f7f8ce77d7c27cd617f9
Pedro Arciniegas <parciniegas@hotmail.com>
2023-04-11 15:59:05 -0500

    Add Notifications project

---
+++ Notifications/Api/ClientController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Notifications.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello from ClientController");
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            return Ok($"Hello from ClientController with id {id}");
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] string client)
+        {
+            return StatusCode(StatusCodes.Status201Created);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] string value)
+        {
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            return Ok();
+        }
+    }
+}
